2997fd2a894701dbc23ab3fe4d5684bc3a6146e1
pistolla <pinkissme@gmail.com>
2022-12-21 19:19:02 +0300

    added required packages

---
+++ app/Http/Livewire/MultipleTabs.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class MultipleTabs extends Component
+{
+    public function render()
+    {
+        return view('livewire.multiple-tabs');
+    }
+}
